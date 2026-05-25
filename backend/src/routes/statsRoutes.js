b3a2a6182e1240f2101da4a88f0257a0b0411e0a
kenshadoow1810-design <kenshadoow1810@gmail.com>
2026-05-25 19:19:41 -0400

    Update statsRoutes.js

--- backend/src/routes/statsRoutes.js
+++ backend/src/routes/statsRoutes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { getTeams, getTeamById, getPlayers, getPlayerById, getChampionStats, getTotalPlayersCount, getLastUpdateTime } = require('../controllers/statsController');
 
-router.get('/api/teams', getTeams);
-router.get('/api/teams/:league', getTeams);
-router.get('/api/team/:id', getTeamById);
+router.get('/teams', getTeams);
+router.get('/teams/:league', getTeams);
+router.get('/team/:id', getTeamById);
 
-router.get('/api/api/players', getPlayers);
-router.get('/api/players/:league', getPlayers);
-router.get('/api/player/:id', getPlayerById);
+router.get('/players', getPlayers);
+router.get('/players/:league', getPlayers);
+router.get('/player/:id', getPlayerById);
 
-router.get('/api/champions', getChampionStats);
+router.get('/champions', getChampionStats);
 
 router.get('/total-players', getTotalPlayersCount);
 router.get('/last-update', getLastUpdateTime);
