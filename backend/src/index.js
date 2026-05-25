c4aff179de8761fe65bbaf56cd88dc7c6283544a
kenshadoow1810-design <kenshadoow1810@gmail.com>
2026-05-25 16:58:33 -0400

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const scheduleRoutes = require('./routes/scheduleRoutes');
 const { runExtraction } = require('./services/dataPipeline');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
